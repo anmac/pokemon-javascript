eb820949f953169624393f55e4bc09bed65c241b
Miguel Maza <anmac_tec@outlook.com>
2023-07-25 12:12:00 -0500

    feat(script): add button to reload and listeners to cards

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const random = Math.floor(Math.random() * 100) + 1;
 const URL = "https://pokeapi.co/api/v2/pokemon/";
-const URL_LIMITED = URL + "?offset=" + random + "&limit=20";
+const URL_SPECIES = "https://pokeapi.co/api/v2/pokemon-species/";
 
 function getPokemon(generator) {
+  const randomQuery = Math.floor(Math.random() * 100) + 1;
+  const URL_LIMITED = URL + "?offset=" + randomQuery + "&limit=20";
   fetch(URL_LIMITED)
     .then((response) => response.json())
     .then((data) => {
@@ -17,32 +18,39 @@ async function getPokemonDetails(name) {
   return data;
 }
 
-function formatName(name) {
+async function getPokemonSpeciesDetails(name) {
+  const response = await fetch(URL_SPECIES + name);
+  const data = await response.json();
+  return data;
+}
+
+function capitalizeName(name) {
   return name.slice(0, 1).toUpperCase() + name.slice(1);
 }
 
-getPokemon(async (data) => {
+async function createPokemonCards(data) {
   const fragment = document.createDocumentFragment();
 
   for (const pokemon of data.results) {
-    console.log(pokemon);
+    // console.log(pokemon);
     const details = await getPokemonDetails(pokemon.name);
-    console.log(details);
+    // console.log(details);
 
     const card = document.createElement("article");
     card.classList.add("card");
+    card.addEventListener("click", getPokemonSpecieDetailAndCreateCard);
 
     const figure = document.createElement("figure");
     figure.classList.add("card__figure");
 
     const img = document.createElement("img");
     img.src = details.sprites.other["official-artwork"].front_default;
-    img.alt = formatName(pokemon.name);
+    img.alt = capitalizeName(pokemon.name);
     img.classList.add("card__img");
 
     const figcaption = document.createElement("figcaption");
     figcaption.classList.add("card__caption");
-    figcaption.textContent = formatName(pokemon.name);
+    figcaption.textContent = capitalizeName(pokemon.name);
 
     figure.appendChild(img);
     figure.appendChild(figcaption);
@@ -52,5 +60,38 @@ getPokemon(async (data) => {
   }
 
   const section = document.querySelector(".section");
+  section.innerHTML = "";
   section.appendChild(fragment);
+  const spinner = document.querySelector(".material-symbols-outlined");
+  spinner.classList.remove("loading");
+}
+
+async function createSpeciesDetails(data) {}
+
+function getPokemonDataAndCreateCards() {
+  const spinner = document.querySelector(".material-symbols-outlined");
+  spinner.classList.add("loading");
+
+  getPokemon(async (data) => {
+    await createPokemonCards(data);
+  });
+}
+
+function getPokemonSpecieDetailAndCreateCard(event) {
+  const card = event.currentTarget;
+  const pokemonName = card
+    .querySelector(".card__caption")
+    .textContent.toLowerCase();
+  createSpeciesDetails(pokemonName);
+}
+
+getPokemonDataAndCreateCards();
+
+document
+  .getElementById("reload")
+  .addEventListener("click", getPokemonDataAndCreateCards);
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  card.addEventListener("click", getPokemonSpecieDetailAndCreateCard);
 });
