888f6d58bf65efda1a6f209f535c5be9045388d9
Miguel Maza <anmac_tec@outlook.com>
2023-07-25 19:30:06 -0500

    refactor(script): split methods and rename them

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
-const URL = "https://pokeapi.co/api/v2/pokemon/";
-const URL_SPECIES = "https://pokeapi.co/api/v2/pokemon-species/";
+const POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+const POKEAPI_SPECIES_BASE_URL = "https://pokeapi.co/api/v2/pokemon-species/";
 
-function getPokemon(generator) {
-  const randomQuery = Math.floor(Math.random() * 100) + 1;
-  const URL_LIMITED = URL + "?offset=" + randomQuery + "&limit=20";
-  fetch(URL_LIMITED)
-    .then((response) => response.json())
-    .then((data) => {
-      generator(data);
-    })
-    .catch((err) => console.error("Pokemon not found: " + err));
+function getRandomOffset() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+function capitalizeName(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+async function getPokemonList(offset, limit) {
+  const response = await fetch(
+    POKEAPI_BASE_URL + `?offset=${offset}&limit=${limit}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch Pokemon list");
+  }
+  const data = await response.json();
+  return data.results;
 }
 
 async function getPokemonDetails(name) {
-  const response = await fetch(URL + name);
+  const response = await fetch(POKEAPI_BASE_URL + name);
+  if (!response.ok) {
+    throw new Error("Failed to fetch Pokemon details");
+  }
   const data = await response.json();
   return data;
 }
 
 async function getPokemonSpeciesDetails(name) {
-  const response = await fetch(URL_SPECIES + name);
+  const response = await fetch(POKEAPI_SPECIES_BASE_URL + name);
+  if (!response.ok) {
+    throw new Error("Failed to fetch Pokemon species details");
+  }
   const data = await response.json();
   return data;
 }
 
-function capitalizeName(name) {
-  return name.slice(0, 1).toUpperCase() + name.slice(1);
-}
-
 async function createPokemonCards(data) {
   const fragment = document.createDocumentFragment();
+  const section = document.querySelector(".section");
+  section.innerHTML = "";
 
-  for (const pokemon of data.results) {
+  for (const pokemon of data) {
     const details = await getPokemonDetails(pokemon.name);
 
     const card = document.createElement("article");
     card.classList.add("card");
-    card.addEventListener("click", getPokemonSpecieDetailAndCreateCard);
+    card.addEventListener("click", () =>
+      showPokemonSpeciesDetails(
+        pokemon.name,
+        details.sprites.other["official-artwork"].front_default
+      )
+    );
 
     const figure = document.createElement("figure");
     figure.classList.add("card__figure");
@@ -57,67 +74,65 @@ async function createPokemonCards(data) {
     fragment.appendChild(card);
   }
 
-  const section = document.querySelector(".section");
-  section.innerHTML = "";
-  section.appendChild(fragment);
-  const spinner = document.querySelector(".material-symbols-outlined");
-  spinner.classList.remove("loading");
-}
-
-async function createSpeciesDetailsCards(pokemonName, img) {
-  const pokemonDetail = await getPokemonSpeciesDetails(pokemonName);
-
-  const article = `
-    <article>
-      <h3 class="detail__title">${pokemonName}</h3>
-      <ul class="detail__list">
-        <li class="item"><span class="item__bold">Color: </span>${pokemonDetail.color.name}</li>
-        <li class="item"><span class="item__bold">Habitat: </span>${pokemonDetail.habitat.name}</li>
-        <li class="item"><span class="item__bold">Species: </span>${pokemonDetail["egg_groups"][0].name}</li>
-        <li class="item"><span class="item__bold">Shape: </span>${pokemonDetail.shape.name}</li>
-        <li class="item"><span class="item__bold">Description: </span>${pokemonDetail["flavor_text_entries"][0]["flavor_text"]}</li>
-      </ul>
-    </article>
-  `;
-
-  const image = `
-    <figure>
-      <img
-        src="${img}"
-        alt="${pokemonName}"
-      />
-    </figure>
-  `;
-
-  const fragment = document.createDocumentFragment();
-  const articleElement = document
-    .createRange()
-    .createContextualFragment(article);
-  const imageElement = document.createRange().createContextualFragment(image);
-  fragment.appendChild(articleElement);
-  fragment.appendChild(imageElement);
-
-  const section = document.querySelector(".detail");
-  section.innerHTML = "";
   section.appendChild(fragment);
 }
 
-function getPokemonDataAndCreateCards() {
+async function showPokemonSpeciesDetails(pokemonName, imgSrc) {
+  const pokemonSpeciesDetails = await getPokemonSpeciesDetails(pokemonName);
+  const detailSection = document.querySelector(".detail");
+  detailSection.innerHTML = "";
+
+  const article = document.createElement("article");
+  const h3 = document.createElement("h3");
+  const ul = document.createElement("ul");
+
+  h3.classList.add("detail__title");
+  h3.textContent = capitalizeName(pokemonName);
+
+  const details = [
+    { label: "Color", value: pokemonSpeciesDetails.color.name },
+    { label: "Habitat", value: pokemonSpeciesDetails.habitat.name },
+    { label: "Species", value: pokemonSpeciesDetails["egg_groups"][0].name },
+    { label: "Shape", value: pokemonSpeciesDetails.shape.name },
+    {
+      label: "Description",
+      value: pokemonSpeciesDetails["flavor_text_entries"][0]["flavor_text"],
+    },
+  ];
+
+  details.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("item");
+    li.innerHTML = `<span class="item__bold">${item.label}: </span>${item.value}`;
+    ul.appendChild(li);
+  });
+
+  article.appendChild(h3);
+  article.appendChild(ul);
+  detailSection.appendChild(article);
+
+  const image = document.createElement("figure");
+  const img = document.createElement("img");
+  img.src = imgSrc;
+  img.alt = capitalizeName(pokemonName);
+  image.appendChild(img);
+  detailSection.appendChild(image);
+}
+
+async function getPokemonDataAndCreateCards() {
   const spinner = document.querySelector(".material-symbols-outlined");
   spinner.classList.add("loading");
 
-  getPokemon(async (data) => {
-    await createPokemonCards(data);
-  });
-}
-
-function getPokemonSpecieDetailAndCreateCard(event) {
-  const card = event.currentTarget;
-  const pokemonName = card
-    .querySelector(".card__caption")
-    .textContent.toLowerCase();
-  const imgSrc = card.querySelector(".card__img").getAttribute("src");
-  createSpeciesDetailsCards(pokemonName, imgSrc);
+  try {
+    const offset = getRandomOffset();
+    const limit = 20;
+    const pokemonList = await getPokemonList(offset, limit);
+    await createPokemonCards(pokemonList);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    spinner.classList.remove("loading");
+  }
 }
 
 getPokemonDataAndCreateCards();
@@ -125,8 +140,3 @@ getPokemonDataAndCreateCards();
 document
   .getElementById("reload")
   .addEventListener("click", getPokemonDataAndCreateCards);
-
-const cards = document.querySelectorAll(".card");
-cards.forEach((card) => {
-  card.addEventListener("click", getPokemonSpecieDetailAndCreateCard);
-});
